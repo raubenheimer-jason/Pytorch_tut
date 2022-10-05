635aff6d02ecf3054e140210470bc0ef7663bc14
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-10-05 09:46:53 +0100

    video4 done - standard NN

--- pytorch_tut/video4.py
+++ pytorch_tut/video4.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
 
+import torch.optim as optim
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -27,15 +29,6 @@ class Net(nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-def testing():
-    net = Net()
-
-    X = torch.rand((28, 28))
-    X = X.view(-1, 28*28)
-    output = net(X)
-    print(output)
-
-
 def main():
     train = datasets.MNIST("", train=True, download=True,
                            transform=transforms.Compose([transforms.ToTensor()]))
@@ -46,7 +39,41 @@ def main():
     trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
     testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
 
+    net = Net()
+
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+    EPOCHS = 3
+
+    for epoch in range(EPOCHS):
+        for data in trainset:
+            # data is a batch of featuresets and lavels
+            X, y = data
+            net.zero_grad()
+            output = net(X.view(-1, 28*28))
+            # if we had 1 hot vector then use mean squared error
+            loss = F.nll_loss(output, y)
+            loss.backward()
+            optimizer.step()  # actually adjusts the weights
+        print(loss)
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in trainset:
+            X, y = data
+            output = net(X.view(-1, 784))
+            for idx, i in enumerate(output):
+                if torch.argmax(i) == y[idx]:
+                    correct += 1
+                total += 1
+
+    print("Accuracy: ", round(correct/total, 3))
+
+    plt.imshow(X[0].view(28, 28))
+    plt.show()
+    print(f"Label: {torch.argmax(net(X[0].view(-1, 784))[0])}")
+
 
 if __name__ == "__main__":
-    # main()
-    testing()
+    main()
